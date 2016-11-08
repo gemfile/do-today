046e2cce6d0d555cd3f3fc9e2ff814c5f2f0f49d
박기용 <usebydriving@gmail.com>
2016-11-09 04:16:05 +0900

    Default setup of android notification.

--- android/app/src/main/java/com/todo_today/notification/NotificationModule.java
+++ android/app/src/main/java/com/todo_today/notification/NotificationModule.java
@@ -1,6 +1,7 @@
 package com.todo_today.notification;
 
 import android.app.Activity;
+import android.app.Application;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -9,15 +10,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-import android.widget.RemoteViews;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.todo_today.MainActivity;
 import com.todo_today.R;
-
-import java.text.DateFormat;
-import java.util.Date;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
 
@@ -28,6 +26,7 @@ import static android.content.Context.NOTIFICATION_SERVICE;
 public class NotificationModule extends ReactContextBaseJavaModule {
 
     private Context applicationContext = null;
+    private int numMessages = 0;
 
     public NotificationModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -43,47 +42,55 @@ public class NotificationModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void create() {
         final Activity applicationActivity = getCurrentActivity();
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(applicationContext);
 
-        Intent resultIntent = new Intent(applicationContext, NotificationReceiver.class);
+        Intent resultIntent = new Intent(applicationContext, MainActivity.class);
         resultIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        PendingIntent resultPendingIntent =
-                PendingIntent.getActivity(
-                        applicationContext,
-                        0,
-                        resultIntent,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
 
-        builder.setContentIntent(resultPendingIntent);
-        builder.setTicker("Hi, ticker");
-        builder.setSmallIcon(R.mipmap.ic_stat_custom);
-        builder.setAutoCancel(true);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(applicationContext)
+            .setContentIntent(
+                PendingIntent.getActivity(
+                    applicationContext,
+                    0,
+                    resultIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+            )
+            .setTicker("Hi, ticker")
+            .setSmallIcon(R.mipmap.ic_stat_custom)
+            .setContentTitle("Hi, title")
+            .setContentText("I'm text." + numMessages)
+            .setNumber(numMessages++)
+            .setAutoCancel(true);
+
 
         Notification notification = builder.build();
 
-        RemoteViews contentView = new RemoteViews(applicationActivity.getPackageName(), R.layout.notification);
-        final String time = DateFormat.getTimeInstance().format(new Date()).toString();
-        final String text = applicationActivity.getResources().getString(R.string.collapsed, time);
-        contentView.setTextViewText(R.id.textView, text);
-        notification.contentView = contentView;
+//        RemoteViews contentView = new RemoteViews(applicationActivity.getPackageName(), R.layout.notification);
+//        final String time = DateFormat.getTimeInstance().format(new Date()).toString();
+//        final String text = applicationActivity.getResources().getString(R.string.collapsed, time);
+//        contentView.setTextViewText(R.id.textView, text);
+//        notification.contentView = contentView;
 
         NotificationManager notifyManager = (NotificationManager) applicationActivity.getSystemService(NOTIFICATION_SERVICE);
-        notifyManager.notify(0, notification);
+        notifyManager.notify(1, notification);
     }
 }
 
 class NotificationReceiver extends BroadcastReceiver {
+    final static String NOTIFICATION_ID = "notificationId";
+    static final String LOG_TAG = "NotificationAndroid";
+
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i("hi", "welcome!");
-//        int id = intent.getIntExtra(NOTIFICATION_ID, 0);
-//        long currentTime = System.currentTimeMillis();
-//
-//        Log.i(LOG_TAG, "NotificationPublisher: Prepare To Publish: " + id + ", Now Time: " + currentTime);
-//
-//        Application applicationContext = (Application) context.getApplicationContext();
-//
+        int id = intent.getIntExtra(NOTIFICATION_ID, 0);
+        long currentTime = System.currentTimeMillis();
+
+        Log.i(LOG_TAG, "NotificationPublisher: Prepare To Publish: " + id + ", Now Time: " + currentTime);
+
+        Application applicationContext = (Application) context.getApplicationContext();
+
 //        new RNPushNotificationHelper(applicationContext)
 //                .sendNotification(intent.getExtras());
     }
